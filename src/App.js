2ffe2d97c2b3d69e9f9295488ef0f7dc1d7adbaa
Akshaya-0410 <akshayathatakula@gmail.com>
2023-10-23 10:56:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+ 
+      <About />
+      <Contact />
+    </main>
+  );
+}
